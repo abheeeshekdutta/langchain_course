f86b9a61c762d64f4ba4f9ae1fa40254cc11afa8
Abhishek Dutta <abhishek.dutta1337@gmail.com>
2023-06-22 23:35:26 +0100

    Adding new files

---
+++ src/llms and langchain/llms_basics.py
@@ -0,0 +1,52 @@
+import dotenv
+
+from langchain import PromptTemplate
+
+template = """Question: {question}
+
+Answer: """
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+# user question
+question = "What is the capital city of France?"
+
+from langchain import HuggingFaceHub, LLMChain
+
+# initialize Hub LLM
+hub_llm = HuggingFaceHub(
+    repo_id="google/flan-t5-large", model_kwargs={"temperature": 0}
+)
+
+# create prompt template > LLM chain
+llm_chain = LLMChain(prompt=prompt, llm=hub_llm)
+
+# ask the user question about the capital of France
+print(llm_chain.run(question))
+
+qa = [
+    {"question": "What is the capital city of France?"},
+    {"question": "What is the largest mammal on Earth?"},
+    {"question": "Which gas is most abundant in Earth's atmosphere?"},
+    {"question": "What color is a ripe banana?"},
+]
+res = llm_chain.generate(qa)
+
+multi_template = """Answer the following questions one at a time.
+
+Questions:
+{questions}
+
+Answers:
+"""
+long_prompt = PromptTemplate(template=multi_template, input_variables=["questions"])
+
+llm_chain = LLMChain(prompt=long_prompt, llm=hub_llm)
+
+qs_str = (
+    "What is the capital city of France?"
+    + "\n"
+    + "What is the largest mammal on Earth?"
+    + "Which gas is most abundant in Earth's atmosphere?"
+    + "What color is a ripe banana?"
+)
+llm_chain.run(qs_str)
